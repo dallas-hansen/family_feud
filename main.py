84281f0781ec3e5d1a68871912f1b0bd549829f3
Dallas <dallashansen.work@gmail.com>
2023-11-27 13:00:24 -0500

    Can fix everything at this point, but I'm about to fuck it up lol

--- main.py
+++ main.py
@@ -5,22 +5,25 @@ from time import sleep
 from threading import Thread
 
 
-
-
 class Game:
+    game_over = False
+    #TODO: implement a fast money round
+    fast_money = False
     def __init__(self):
         self.seed = 0
+        self.previous_seeds = []
         self.pool = []
         self.question = ''
         self.answers = {}
         self.num_of_answers = 0
-        self.correct = []
+        self.correct_guesses = []
         self.points = 0
         self.countdown = 45
         self.host = ''
 
 
     def timer(self):
+        # Countdown timer
         seconds = self.countdown
         while seconds:
             timeformat = f'\t:{seconds:02d}'
@@ -30,10 +33,14 @@ class Game:
 
 
     def set_seed(self):
-        self.seed = random.randint(0, len(self.pool) - 1)
+        # Sets seed to a random number
+        while self.seed in self.previous_seeds:
+            self.seed = random.randint(0, len(self.pool) - 1)
+        self.previous_seeds.append(self.seed)
     
 
     def fill_up_pool(self, pool_size=50):
+        # Creates pool of questions. Pool_size is the number of questions in the pool.        
         json_files = glob.glob(r'questions_and_answers\json\*.json')
         for file_path in json_files:
             with open(file_path, 'r') as file:
@@ -46,60 +53,80 @@ class Game:
     
 
     def get_question(self):
+        # Set seed and get question
         self.set_seed()
         self.question = self.pool[self.seed]["Question"]
 
     
     def get_answers(self):
+        # Gets number of answers and parses answers from seed
+        self.answers = {}
         seed = self.pool[self.seed]
         self.num_of_answers = (len(seed) - 1) // 2
         for num in range(self.num_of_answers):
             answer = seed[f'Answer {num + 1}']
             self.answers[f'{answer}'] = seed[f'#{num + 1}']
-
-
-    def display(self, guess = '', round_over=False):
-        answers = sorted(self.answers.items(), reverse=True, key=lambda item: int(item[1]))
+        self.answers = sorted(self.answers.items(), reverse=True, key=lambda item: int(item[1])) 
+        
+        
+    def display(self, round_over=False):
+        guess = self.host.guess
         word_box = '-' * 30
         points_box = ('-' * 4)
         print(f'\nTop {self.num_of_answers} answers on the board\n')
         print(f'{self.question}\n')
-        self.big_x()
         for i in range(self.num_of_answers):
-            len_points_str = len(str(answers[i][1]))
-            answer = answers[i][0]
-            points = answers[i][1]
+            len_points_str = len(str(self.answers[i][1]))
+            answer = self.answers[i][0]
+            points = self.answers[i][1]
             # prints top of box
             print(' ' + word_box + ' ' + points_box)
             print(f'| {i + 1}. ', end='')
             # prints sides and middle of box
             if self.survey_says(answer, points, guess) or round_over:
                 print(answer + '|'.rjust((len(word_box)-3) - len(answer)) + ' ' + str(points) + '|'.rjust(4 - len_points_str))
-                self.host.guess_outcome == 'Correct'
             else:
                 print((' ' * (len(word_box) - 4)) + '|' + '|'.rjust(5))
                 
         print(' ' + word_box + ' ' + points_box)
+        self.big_x()
         
     
-    def survey_says(self, answer, points, guess = ''):
+    def survey_says(self, answer: str, points: int, guess = '') -> bool:
+        """
+        Checks if the guess is correct or not.
+
+        Args:
+            answer (str): The answer to the question.
+            points (int): The amount of points it's worth.
+            guess (str, optional): Current guess. Defaults to ''.
+
+        Returns:
+            bool: Whether or not the guess was correct.
+        """
         # Sorts dictionary into a list of tuples based off the value
-        guess_lower = guess.lower()
-        answer = answer.lower()
-        if answer in self.correct:
-            return True                                
-        elif guess_lower == answer:
-            self.correct.append(guess_lower)
-            self.points += int(points)
-            return True
-        elif self.host.guess_outcome == 'Correct':
-            return False 
-        else:
-            self.host.guess_outcome == 'Wrong Answer'
-            return False
+        # guess_lower = guess.lower()
+        # answer = answer.lower()
+        # if answer in self.correct_guesses:
+        #     return True                                
+        # elif guess_lower == answer:
+        #     self.correct_guesses.append(guess_lower)
+        #     self.points += int(points)
+        #     self.host.guess_outcome = 'Correct'
+        #     return True
+        # elif self.host.guess_outcome == 'Correct':
+        #     return False 
+        # else:
+        #     self.host.guess_outcome = 'Wrong Answer'
+        #     return False
+        guess_lower = self.host.guess.lower()
         
+        for i, item in enumerate(self.answers):
+            if item[0] == guess_lower:
+                self.correct_guesses.append(i)
     
     def big_x(self):
+        # Prints a big X for each strike
         top = '\/ '
         bottom = '/\\ '
         print(top * self.host.current_team.strikes)
@@ -124,13 +151,12 @@ class Family:
             self.members.append(input(f'Who is in position {i + 1}?: '))
     
     def guess(self):
-        guess = input()
+        guess = input(f'{self.name}, what is your guess?: ')
         return guess
     
     
 
 class Host:
-    # Trying to figure out guess outcome and applying strikes properly
     def __init__(self, game, family1, family2='', name='Heave Starvey'):
         self.name = name
         self.game = game
@@ -139,10 +165,19 @@ class Host:
         self.current_team = ''
         self.guess = ''
         self.guess_outcome = 'unknown'
+        self.strikes_available = 3
+        self.is_stealing = False
+        
+    
+    def reset(self):
+        self.current_team.strikes = 0
+        self.strikes_available = 3
 
     
     def next_round(self):
-        if self.current_team == self.team_1:
+        if self.is_stealing:
+            self.is_stealing = False
+        elif self.current_team == self.team_1:
             self.current_team = self.team_2
         else:
             self.current_team = self.team_1
@@ -150,6 +185,8 @@ class Host:
     
     def chance_to_steal(self):
         self.next_round()
+        self.is_stealing = True
+        self.strikes_available = 1
         print(f'Now Team {self.current_team.name} has a chance to steal!')
 
 
@@ -159,10 +196,7 @@ class Host:
 
 
     def check_guess(self):
-        self.game.display(self.guess)
-        print(f'Guess outcome= {self.guess_outcome}')
         if self.guess_outcome == 'Wrong Answer':
-            print(f'guess_outcome = {self.guess_outcome}')
             self.add_strike()
 
 
@@ -175,8 +209,6 @@ class Host:
                 self.chance_to_steal()
 
 def main():
-    end_condition = False
-    force_end = ''
     team_1 = Family(input('What is your team name?\n'))
     # team_1.add_players()
     team_2 = Family(input('What is your team name?\n'))
@@ -186,14 +218,15 @@ def main():
     host = Host(game, team_1, team_2)
     game.host = host
 
-    while end_condition is False or force_end != 'stop':
+    while not game.game_over:
         host.next_round()
+        host.reset()
         game.get_question()
         game.get_answers()
-        while host.current_team.strikes != 3:
+        while host.current_team.strikes != host.strikes_available:
             game.display()
             print(game.answers)
-            force_end = host.get_guess()
+            host.get_guess()
             host.check_guess()
 
 
